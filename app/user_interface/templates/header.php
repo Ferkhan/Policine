3c1a8d772aa15785181cf795d99fa0f617077ee0
Ariel Sánchez <arielss77cc@gmail.com>
2024-02-22 21:51:18 -0500

    Implementación del header y footer del proyecto Policine.

--- app/user_interface/templates/header.php
+++ app/user_interface/templates/header.php
@@ -1,16 +1,35 @@
+<?php
+$logo = ""; // Ruta al logo que vamos a utilizar.
+$titulo = "Ubicación";
+$menu = [
+    "Fecha" => "#",
+    "Películas" => "#"
+];
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Policine</title>
+    <title>Policine <?php echo $titulo; ?></title>
 </head>
 <body>
-    <header>
-
-
-
-
-    
+    <header style="background-color: #000; color: #fff; padding: 20px;">
+        <div style="display: flex; justify-content: space-between;">
+            <div>
+                <img src="<?php echo $logo; ?>" alt="Logo" style="height: 50px;">
+                <span style="margin-left: 20px;"><?php echo $titulo; ?></span>
+            </div>
+            <nav>
+                <ul style="list-style-type: none; display: flex; gap: 20px;">
+                    <?php foreach ($menu as $item => $link): ?>
+                        <li><a href="<?php echo $link; ?>"><?php echo $item; ?></a></li>
+                    <?php endforeach; ?>
+                    <li><a href="inicio_sesion.php" style="background-color: #ff0; padding: 10px 20px;">Iniciar sesión</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
+</body>
+</html>
+
    
