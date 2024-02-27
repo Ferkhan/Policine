c7c2884f912ded64bf9c73959d0c7a8cba37e4d8
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2024-02-27 14:59:37 -0500

    arreglar header y footer

--- app/user_interface/templates/header.php
+++ app/user_interface/templates/header.php
@@ -1,25 +1,27 @@
 <?php
 
 $enlace = "http://192.168.1.19/Policine/app/";
-$logo = ""; // Ruta al logo que vamos a utilizar.
+$logo = "http://192.168.1.19/Policine/resources/images/epn.png"; // Ruta al logo que vamos a utilizar.
 $titulo = "Policine";
 $menu = [
     // "Fecha" => "#",
     "Peliculas" => "#"
 ];
 ?>
-
+   
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo; ?></title>
 </head>
 <body>
-    <header style="background-color: #000; color: #fff; padding: 20px;">
+    <header style="background-color: #000; color: #fff; padding: 10px;">
         <div style="display: flex; justify-content: space-between;">
             <div>
-                <img src="<?php echo $logo; ?>" alt="Logo" style="height: 50px;">
-                <span style="margin-left: 20px;"><?php echo $titulo; ?></span>
+                <img src="<?php echo $logo; ?>" alt="Logo" style="height: 50px;margin-bottom: 0;">
+                <span style="margin-left: 0px; font-size: 40px; font-weight: bold;"><?php echo $titulo; ?></span>
             </div>
             <nav>
                 <ul style="list-style-type: none; display: flex; gap: 20px;">
@@ -32,6 +34,5 @@ $menu = [
             </nav>
         </div>
     </header>
-</body>
 
    
