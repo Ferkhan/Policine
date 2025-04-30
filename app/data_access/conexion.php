35e8a05351230ba3a707ed3bb3ec9702c64edf95
F 3 R K H 4 N <133190285+Ferkhan@users.noreply.github.com>
2025-04-30 04:35:28 -0500

    coneccion a postgres en railway

--- app/data_access/conexion.php
+++ app/data_access/conexion.php
@@ -1,16 +1,23 @@
 <?php
 class Conexion {
-    public $host       = "localhost";
-    public $dbname     = "DB_Policine";
-    public $username   = "ferkhan";
-    public $password   = "ferkhan";
+    // public $host       = "localhost";
+    // public $dbname     = "DB_Policine";
+    // public $username   = "ferkhan";
+    // public $password   = "ferkhan";
+    // public $conn       = null;
+
+    public $host       = "yamabiko.proxy.rlwy.net";
+    public $dbname     = "railway";
+    public $username   = "postgres";
+    public $password   = "EQdgiIzhThISqvEbupPkpSxSCWHrwZiw";
+    public $port       = "51372";
     public $conn       = null;
 
     // Método que permite realizarla la conexión a la base de datos
     public static function obtenerConexion() {
         try {
             $connection = new Conexion();
-            $connection->conn = new PDO ("pgsql: host = $connection->host; dbname = $connection->dbname", $connection->username, $connection->password);
+            $connection->conn = new PDO ("pgsql: host = $connection->host; port = $connection->port; dbname = $connection->dbname", $connection->username, $connection->password);
             // echo "Conexión exitosa";
             return $connection->conn;
         } catch(PDOException $exp) {
