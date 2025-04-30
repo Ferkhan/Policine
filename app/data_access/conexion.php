21467c4ba23e1e33f40556ee08e9b06243566db8
F 3 R K H 4 N <133190285+Ferkhan@users.noreply.github.com>
2025-04-30 12:32:04 -0500

    cambiar variables de entorno

--- app/data_access/conexion.php
+++ app/data_access/conexion.php
@@ -13,11 +13,11 @@ class Conexion {
     // public $DB_PORT       =    $_ENV=["51372"];
 
 
-    public $DB_HOST       = $_ENV["DB_HOST"];
-    public $DB_NAME       = $_ENV["DB_NAME"];
-    public $DB_USER       = $_ENV["DB_USER"];
-    public $DB_PASSWORD   = $_ENV["DB_PASSWORD"];
-    public $DB_PORT       = $_ENV["DB_PORT"];
+    public $DB_HOST       = getenv("DB_HOST");
+    public $DB_NAME       = getenv("DB_NAME");
+    public $DB_USER       = getenv("DB_USER");
+    public $DB_PASSWORD   = getenv("DB_PASSWORD");
+    public $DB_PORT       = getenv("DB_PORT");
     public $conn       = null;
 
     // Método que permite realizarla la conexión a la base de datos
