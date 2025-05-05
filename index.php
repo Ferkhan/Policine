2bac59d77edd545006135baa22e03f182b5bbcee
F 3 R K H 4 N <133190285+Ferkhan@users.noreply.github.com>
2025-05-05 12:21:50 -0500

    crear archivo compose con volumen

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-session_start();
-
-if (isset($_SESSION['usuario_id'])) {
-    header('Location: app/user_interface/cartelera.php');
-    die();
-}
-
-header('Location: app/user_interface/inicio_sesion.php');
-die();
+<?php echo "Hola !"; ?>
