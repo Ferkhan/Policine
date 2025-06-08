310d31c3ba15911cfbe480f1b474e785ccb0e275
F 3 R K H 4 N <133190285+Ferkhan@users.noreply.github.com>
2025-06-07 23:57:04 -0500

    version postgres en Neon

--- app/data_access/conexion.php
+++ app/data_access/conexion.php
@@ -1,23 +1,10 @@
 <?php
 class Conexion {
-    // public $host       = "localhost";
-    // public $dbname     = "DB_Policine";
-    // public $username   = "ferkhan";
-    // public $password   = "ferkhan";
-    // public $conn       = null;
-
     public $DB_HOST     = "ep-odd-meadow-ac42xgdx-pooler.sa-east-1.aws.neon.tech";
-    // public $DB_HOST     = "policine-poli-1";
     public $DB_NAME     = "neondb";
     public $DB_USER     = "neondb_owner";
     public $DB_PASSWORD = "npg_AxTL2prJecQ7";
     public $DB_PORT     = "5432";
-
-    // public $DB_HOST       = getenv("DB_HOST");
-    // public $DB_NAME       = getenv("DB_NAME");
-    // public $DB_USER       = getenv("DB_USER");
-    // public $DB_PASSWORD   = getenv("DB_PASSWORD");
-    // public $DB_PORT       = getenv("DB_PORT");
     public $conn       = null;
 
     // Método que permite realizar la conexión a la base de datos
@@ -25,16 +12,8 @@ class Conexion {
         try {
             $connection = new Conexion();
 
-            // Obtener el endpoint ID (la primera parte del hostname)
-            $endpointId = explode('.', $connection->DB_HOST)[0];
+            $dsn = "pgsql:host={$connection->DB_HOST};port={$connection->DB_PORT};dbname={$connection->DB_NAME};sslmode=require";
 
-            // Codificar correctamente el parámetro options con URL encoding
-            $options = rawurlencode("--endpoint=$endpointId");
-
-            // Construir el DSN con sslmode=require y el parámetro options
-            $dsn = "pgsql:host={$connection->DB_HOST};port={$connection->DB_PORT};dbname={$connection->DB_NAME};sslmode=require;options=$options";
-
-            // Crear la conexión PDO
             $connection->conn = new PDO($dsn, $connection->DB_USER, $connection->DB_PASSWORD);
             return $connection->conn;
 
@@ -43,8 +22,6 @@ class Conexion {
             return null;
         }
     }
-
-
 }
 
 
